852e8d1d3d8fc5071781356f11a4cd7373788c5b
Casey Caruso <caseykcaruso@gmail.com>
2020-08-02 17:51:38 -0400

    changing get request to https

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function resetForm(val) {
 }
 
 function showHelpfulGif() {
-  $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=helpful', function (result) {
+  $.get('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=helpful', function (result) {
     gif = result.data.image_url;
     $('.terminal').append('<img class="gif" src="' + gif + '""><br>');
   }).then(function () {
